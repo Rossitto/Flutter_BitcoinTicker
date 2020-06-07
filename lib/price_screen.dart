f5f4e7cd9a84a50d278af22d88bfb54d38dd3c00
Rossitto <lucas74rossi@gmail.com>
2020-06-07 16:01:29 -0300

    added a function that create list a of DropDownItems

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,14 @@
 import 'package:flutter/material.dart';
+import 'coin_data.dart';
+
+List<DropdownMenuItem<String>> listDropDownItems() {
+  List<DropdownMenuItem<String>> widgetsList = [];
+  for (String currency in currenciesList) {
+    var newItem = DropdownMenuItem(child: Text(currency), value: currency);
+    widgetsList.add(newItem);
+  }
+  return widgetsList;
+}
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -46,11 +56,7 @@ class _PriceScreenState extends State<PriceScreen> {
             color: Colors.lightBlue,
             child: DropdownButton<String>(
               value: selectedCurrency,
-              items: [
-                DropdownMenuItem(child: Text('USD'), value: 'USD'),
-                DropdownMenuItem(child: Text('EUR'), value: 'EUR'),
-                DropdownMenuItem(child: Text('GBP'), value: 'GBP'),
-              ],
+              items: listDropDownItems(),
               onChanged: (value) {
                 setState(() {
                   selectedCurrency = value;
