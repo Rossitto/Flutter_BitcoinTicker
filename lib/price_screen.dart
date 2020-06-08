74e1005b788524acf9be42d55935be1fd0d9551e
Rossitto <lucas74rossi@gmail.com>
2020-06-07 22:55:53 -0300

    imported dart:io and using Platform

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -42,14 +42,6 @@ class _PriceScreenState extends State<PriceScreen> {
         children: pickerList);
   }
 
-  Widget getPicker() {
-    if (Platform.isIOS) {
-      return iOSPicker();
-    } else if (Platform.isAndroid) {
-      return androidDropdown();
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -86,7 +78,7 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: getPicker(),
+            child: Platform.isIOS ? iOSPicker() : androidDropdown(),
           ),
         ],
       ),
